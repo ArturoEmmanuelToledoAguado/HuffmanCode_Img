ac14ca562468732d517416714d7eb4f9e532e5e0
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-12-03 14:56:20 -0600

    Probabilidad acumulada

--- Huffman.m
+++ Huffman.m
@@ -8,3 +8,24 @@ img=imread('IMG/1.jpg');
 img=rgb2gray(img);
 figure
 imshow(img)
+
+%Tamaño de la imagen
+[f,c]=size(img);
+Tam=f*c;
+
+%Variables para encontrar la probabilidad
+cont=1;
+sig=0;
+
+%Calculo de probabilidad acumulada
+for i=0:255
+    j=img==i;%Preguntamos si el valor de i esta em la matriz de la imagen
+    contador(cont)=sum(j(:));%Apariciones
+    
+    %Matriz de probabilidades
+    prob(cont)=contador(cont)/Tam;
+    sig=sig+prob(cont);
+    Cprob(cont)=sig;
+    cont=cont+1;
+end
+
